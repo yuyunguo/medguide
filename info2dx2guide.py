d27c57adb680f9be72ec2ad5b753df304c2f11d6
yuyunguo <yyg2k4@yahoo.com>
2024-05-03 20:51:28 -0400

    Update info2dx2guide.py
    remove the ipykernel command

--- info2dx2guide.py
+++ info2dx2guide.py
@@ -48,9 +48,9 @@ from pathlib import Path
 if __name__ == "__main__":
     # Remove the CWD from sys.path while we load stuff.
     # This is added back by InteractiveShellApp.init_path()
-    if sys.path[0] == "" or Path(sys.path[0]) == Path.cwd():
-        del sys.path[0]
+    #if sys.path[0] == "" or Path(sys.path[0]) == Path.cwd():
+    #    del sys.path[0]
 
-    from ipykernel import kernelapp as app
+    # from ipykernel import kernelapp as app
 
     app.launch_new_instance()
